72564d916ff12df55f4d1ba4ed56c835289e8da9
mtwichel <marc.twichy@gmail.com>
2021-12-06 20:56:52 -0700

    change launch.json

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,1 +1,2 @@
+export 'cubit/login_cubit.dart';
 export 'view/view.dart';
--- lib/login/view/login_page.dart
+++ lib/login/view/login_page.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ugly_sweater_app/login/login.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocProvider(
+      create: (context) => LoginCubit(),
+      child: const LoginPageView(),
+    );
+  }
+}
+
+class LoginPageView extends StatelessWidget {
+  const LoginPageView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Sign in With Google'),
+          onPressed: () {},
+        ),
+      ),
+    );
   }
 }
